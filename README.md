9d04ab8c2775842335ad63bd22ef1cd77e66aa96
liangrumeng <liangrumeng2015@163.com>
2019-11-04 15:57:16 +0800

    update the pages of login

--- README.md
+++ README.md
@@ -31,3 +31,44 @@ BrowserRouter   【没有#的路由】
 ## 高阶函数、高阶组件
 const WrapLogin = Form.create()(Login)
 export default WrapLogin
+
+## 高阶函数、高阶组件
+`高阶函数`：
+（1）接受函数类型的参数、返回值是函数
+（2）eg：
+    定时器：setTimeout、setInterval()、
+    Promise：Promise(()=>{})  then(()=>{})
+    数组遍历：forEach()、filter()、map()、reduce()、find()、findIndex()
+    函数对象的bind()
+    Form.create()()  /  getFieldDecorator()()
+（3）高阶函数更新动态、更加具有扩展性。
+
+`高阶组件`：
+（1）本质就是一个函数
+（2）接收一个组价（被包装组件），返回一个新的组件（包装组件），包装组件会向被包装组件传入特定属性。
+（3）作用：扩展组件的功能。
+(4)eg:
+    包装Form组件生成一个新的组件：Form(Login)
+    新组件会向Form组件传递一个强大的对象属性：form
+    const WrapLogin = Form.create()(Login)
+(5)高阶组件也是高阶函数：接收一个组件函数，返回是一个新的组件函数。
+
+## 前台表单验证
+(1)声明式实时表单验证
+form.getFieldDecorator('标示名称',{rules:[{}]})(<Input />)
+
+(2)自定义表单验证：
+form.getFieldDecorator('标示名称',{rules:[validator:this.validatorPwd]})(<Input />)
+validatorPwd=(rule,value,callback)=>{
+    callback()  // 成功
+    callback('xxx')   // 验证失败
+}
+
+(3)点击提交时，统一验证
+form.validateFields((err,values)=>{
+    if(!err){
+        console.log('成功',values)
+    }else{
+        console.log('失败');
+    }
+})
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -13,12 +13,37 @@ class Login extends Component {
     }
     handleSubmit=(event)=>{
         // 阻止事件的默认行为
-        event.preventDefault();
-        // 得到form对象
-        const form = this.props.form;
-        const values = form.getFieldsValue();
-        console.log('得到表单里面提交的值：',values);
-    }
+				event.preventDefault();
+				
+				this.props.form.validateFields((err, values) => {
+					if (!err) {
+						console.log('提交表单的数据请求',values);
+					}else{
+						console.log('验证失败')
+					}
+				});
+
+        // // 得到form对象
+        // const form = this.props.form;
+        // const values = form.getFieldsValue();
+        // console.log('得到表单里面提交的值：',values);
+		}
+		// 对密码进行自定义验证，callback必须被调用
+		//  callback()  验证通过    callback('xxx')  验证失败，并指定提示的文本
+		validatorPwd=(rule, value, callback)=>{
+			console.log(rule,value)
+			if(!value){
+				callback('密码必输');
+			}else if(value.length <= 4){
+				callback('密码长度不能小于4')
+			}else if(value.length >= 12){
+				callback('密码的长度不能大于12')
+			}else if(!/^[a-zA-Z0-9_]+$/.test(value)){
+				callback('密码必须是英文、数字、或下划线组成')
+			}else{
+				callback();   // 验证通过
+			}
+		}
     render() {
         const { getFieldDecorator } = this.props.form;
         return (
@@ -30,9 +55,24 @@ class Login extends Component {
                 <div className="login-content">
                     <h2>用户登录</h2>
                     <Form onSubmit={this.handleSubmit} className="login-form">
+											{/* 
+												校验规则：
+												（1）必须输入
+												（2）>=4 且 <=12
+												（3）必须是英文、数字、或下划线组成。
+											*/}
                         <Form.Item>
                             {
-                                getFieldDecorator('username',{})(
+                                getFieldDecorator('username',{
+																	// 声明式验证
+																	rules: [
+																		{ required: true, message: '用户名必须输入' },
+																		{ min: 4, message: '用户名最少4位' },
+																		{ max: 12, message: '用户名不能超过12位' },
+																		{ pattern:/^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字、或下划线组成'}
+                                  ],
+                                  initialValue:'admin'   // 指定初始值
+																})(
                                     <Input
                                         prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                                         placeholder="用户名"
@@ -42,7 +82,11 @@ class Login extends Component {
                         </Form.Item>
                         <Form.Item>
                             {
-                                getFieldDecorator('password',{})(
+                                getFieldDecorator('password',{
+																	rules:[
+																		{validator:this.validatorPwd}
+																	]
+																})(
                                     <Input
                                         prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
                                         type="password"
